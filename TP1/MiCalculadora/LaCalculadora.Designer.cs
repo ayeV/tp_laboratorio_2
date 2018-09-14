4b6efdc391edb3a2d5386705d648664a1044cf25
ayeV <36230640+ayeV@users.noreply.github.com>
2018-09-14 17:44:32 -0300

    Add files via upload

---
+++ TP1/MiCalculadora/LaCalculadora.Designer.cs
@@ -0,0 +1,168 @@
+﻿namespace MiCalculadora
+{
+    partial class LaCalculadora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnOperar = new System.Windows.Forms.Button();
+            this.btnLimpiar = new System.Windows.Forms.Button();
+            this.btnCerrar = new System.Windows.Forms.Button();
+            this.btnConvertirADecimal = new System.Windows.Forms.Button();
+            this.btnConvertirABinario = new System.Windows.Forms.Button();
+            this.cmbOperador = new System.Windows.Forms.ComboBox();
+            this.textNumero1 = new System.Windows.Forms.TextBox();
+            this.textNumero2 = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnOperar
+            // 
+            this.btnOperar.Location = new System.Drawing.Point(50, 221);
+            this.btnOperar.Name = "btnOperar";
+            this.btnOperar.Size = new System.Drawing.Size(94, 34);
+            this.btnOperar.TabIndex = 0;
+            this.btnOperar.Text = "Operar";
+            this.btnOperar.UseVisualStyleBackColor = true;
+            this.btnOperar.Click += new System.EventHandler(this.btnOperar_Click);
+            // 
+            // btnLimpiar
+            // 
+            this.btnLimpiar.Location = new System.Drawing.Point(199, 221);
+            this.btnLimpiar.Name = "btnLimpiar";
+            this.btnLimpiar.Size = new System.Drawing.Size(89, 34);
+            this.btnLimpiar.TabIndex = 1;
+            this.btnLimpiar.Text = "Limpiar";
+            this.btnLimpiar.UseVisualStyleBackColor = true;
+            this.btnLimpiar.Click += new System.EventHandler(this.btnLimpiar_Click);
+            // 
+            // btnCerrar
+            // 
+            this.btnCerrar.Location = new System.Drawing.Point(328, 221);
+            this.btnCerrar.Name = "btnCerrar";
+            this.btnCerrar.Size = new System.Drawing.Size(96, 34);
+            this.btnCerrar.TabIndex = 2;
+            this.btnCerrar.Text = "Cerrar";
+            this.btnCerrar.UseVisualStyleBackColor = true;
+            this.btnCerrar.Click += new System.EventHandler(this.btnCerrar_Click);
+            // 
+            // btnConvertirADecimal
+            // 
+            this.btnConvertirADecimal.Location = new System.Drawing.Point(289, 324);
+            this.btnConvertirADecimal.Name = "btnConvertirADecimal";
+            this.btnConvertirADecimal.Size = new System.Drawing.Size(117, 35);
+            this.btnConvertirADecimal.TabIndex = 3;
+            this.btnConvertirADecimal.Text = "Convertir a decimal";
+            this.btnConvertirADecimal.UseVisualStyleBackColor = true;
+            this.btnConvertirADecimal.Click += new System.EventHandler(this.btnConvertirADecimal_Click);
+            // 
+            // btnConvertirABinario
+            // 
+            this.btnConvertirABinario.Location = new System.Drawing.Point(66, 327);
+            this.btnConvertirABinario.Name = "btnConvertirABinario";
+            this.btnConvertirABinario.Size = new System.Drawing.Size(120, 32);
+            this.btnConvertirABinario.TabIndex = 4;
+            this.btnConvertirABinario.Text = "Convertir a binario";
+            this.btnConvertirABinario.UseVisualStyleBackColor = true;
+            this.btnConvertirABinario.Click += new System.EventHandler(this.btnConvertirABinario_Click);
+            // 
+            // cmbOperador
+            // 
+            this.cmbOperador.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbOperador.FormattingEnabled = true;
+            this.cmbOperador.Items.AddRange(new object[] {
+            "+",
+            "-",
+            "/",
+            "*"});
+            this.cmbOperador.Location = new System.Drawing.Point(177, 101);
+            this.cmbOperador.Name = "cmbOperador";
+            this.cmbOperador.Size = new System.Drawing.Size(94, 21);
+            this.cmbOperador.TabIndex = 6;
+            // 
+            // textNumero1
+            // 
+            this.textNumero1.Location = new System.Drawing.Point(50, 102);
+            this.textNumero1.Name = "textNumero1";
+            this.textNumero1.Size = new System.Drawing.Size(100, 20);
+            this.textNumero1.TabIndex = 8;
+            // 
+            // textNumero2
+            // 
+            this.textNumero2.Location = new System.Drawing.Point(306, 101);
+            this.textNumero2.Name = "textNumero2";
+            this.textNumero2.Size = new System.Drawing.Size(100, 20);
+            this.textNumero2.TabIndex = 9;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(47, 69);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(359, 18);
+            this.lblResultado.TabIndex = 10;
+            this.lblResultado.Text = "resultado";
+            this.lblResultado.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            
+            // 
+            // LaCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(462, 409);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.textNumero2);
+            this.Controls.Add(this.textNumero1);
+            this.Controls.Add(this.cmbOperador);
+            this.Controls.Add(this.btnConvertirABinario);
+            this.Controls.Add(this.btnConvertirADecimal);
+            this.Controls.Add(this.btnCerrar);
+            this.Controls.Add(this.btnLimpiar);
+            this.Controls.Add(this.btnOperar);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "LaCalculadora";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora de Ayelen Valdez del curso 2ºA";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnOperar;
+        private System.Windows.Forms.Button btnLimpiar;
+        private System.Windows.Forms.Button btnCerrar;
+        private System.Windows.Forms.Button btnConvertirADecimal;
+        private System.Windows.Forms.Button btnConvertirABinario;
+        private System.Windows.Forms.ComboBox cmbOperador;
+        private System.Windows.Forms.TextBox textNumero1;
+        private System.Windows.Forms.TextBox textNumero2;
+        private System.Windows.Forms.Label lblResultado;
+    }
+}
---
+++ TP1/MiCalculadora/LaCalculadora.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MiCalculadora
+
+{
+    public partial class LaCalculadora : Form
+    {
+        public LaCalculadora()
+        {
+            InitializeComponent();
+        }
+
+
+        private void btnCerrar_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+            
+        }
+
+        private void btnLimpiar_Click(object sender, EventArgs e)
+        {
+            Limpiar();
+        }
+
+        private void Limpiar()
+        {
+            lblResultado.Text = "";
+            textNumero1.Text = "";
+            textNumero2.Text = "";
+            cmbOperador.Text = "";
+
+        }
+
+
+
+        private void btnOperar_Click(object sender, EventArgs e)
+        {
+            double resutlado = Operar(textNumero1.Text, textNumero2.Text, cmbOperador.SelectedItem.ToString());
+            lblResultado.Text = resutlado.ToString();
+        }
+        private static double Operar(string numero1, string numero2, string operador)
+        {
+            double resultado = Entidades.Calculadora.Operar(new Entidades.Numero(numero1), new Entidades.Numero(numero2), operador);
+
+            return resultado;
+        }
+
+        private void btnConvertirABinario_Click(object sender, EventArgs e)
+        {
+            Entidades.Numero numero1 = new Entidades.Numero(textNumero1.Text);
+            lblResultado.Text = numero1.DecimalBinario(textNumero1.Text);
+
+        }
+
+        private void btnConvertirADecimal_Click(object sender, EventArgs e)
+        {
+            Entidades.Numero numero1 = new Entidades.Numero(textNumero1.Text);
+            lblResultado.Text = numero1.BinarioDecimal(textNumero1.Text);
+        }
+        
+
+
+    }
+}
